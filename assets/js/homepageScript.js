fab3c7e9a8edd1041c84d482d94f56e0460c03a0
Kay <ckay_006@yahoo.com>
2022-10-16 18:00:42 -0400

    changed order of cards, made images same height in cards

--- assets/js/homepageScript.js
+++ assets/js/homepageScript.js
@@ -1,13 +1,21 @@
 var logOutBtn = document.querySelector("#signOut")
+//user clicks logout button
 logOutBtn.addEventListener("click",signOutUser)
 function signOutUser() {
+    //clear e-mail and password and set is localStorage.setItem("login", false)
     localStorage.removeItem("email")
     localStorage.removeItem("password")
     localStorage.setItem("login", false)
+    //redirect to login page
     window.location.href="./index.html"
 }
 
+var welcomeUser = document.querySelector(".welcomeUser")
+welcomeUser.textContent = ("Welcome " + localStorage.getItem("email") + "!")
 
-//user clicks logout button
-//clear e-mail and password and set is   localStorage.setItem("login", false)
-//redirect to login page
+
+//Welcome user when they log in
+// dynamically create a h1 tag and make text.content=localStorage.getItem("email")
+//append to html
+
+
